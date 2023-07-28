6f67d2e4d47944530b705adeee8c280825ae7b49
Sushil Padhi <sushilpadhi559@gmail.com>
2023-07-28 21:26:28 +0530

    Added port env

--- app.js
+++ app.js
@@ -169,6 +169,6 @@ app.get("/:customListname", function(req, res){
 
 
 
-app.listen(3000,() =>{
-    console.log("The server has started on port 3000");
+app.listen(process.env.PORT ||3000,() =>{
+    console.log("The server has started ");
 });
